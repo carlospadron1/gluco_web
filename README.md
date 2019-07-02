576b088cea9bc4d4105ee0745b74b4654869d821
Carlos Padron <c_0509@hotmail.com>
2019-07-01 19:54:25 -0500

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Proyecto gluco web" 
---
+++ public/js/home.js
@@ -0,0 +1,23 @@
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log("con usuario")
+        if (user.metadata.creationTime === user.metadata.lastSignInTime) {
+            // this is a new user
+            console.log('Usuario nuevo');
+        }
+        else {
+            console.log('Usuario registrado previamente');
+        }
+    }
+    else {
+        window.location.replace('index.html')
+        console.log("sin usuario")
+    }
+});
+
+
+
+function logout() {
+    firebase.auth().signOut();
+}
+
