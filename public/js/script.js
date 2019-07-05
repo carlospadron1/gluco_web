1c05893dc1f8597d486ea76d24666fe3c0a8d2cd
Mario Padrón <mario@prointernet.mx>
2019-07-04 23:10:28 -0500

    -Update de log terminado

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+/* animación burger menu */
+$(document).ready(function () {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+    });
+});
+/* animación burger menu */
